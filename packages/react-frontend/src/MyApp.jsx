42e2038dc49ef94dfb11f4dd923c7fe2d0e9b741
briggswilliammurphy <briggswillmurphy@gmail.com>
2025-11-05 16:30:57 -0800

    Added very basic structures for about, home, and profile

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,69 +1,25 @@
-import React, { useState, useEffect } from "react";
-import Table from "./Table";
-import Form from "./Form";
+// src/MyApp.jsx
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import "./main.css"; // make sure your CSS is imported
 
-function MyApp() {
-  const [characters, setCharacters] = useState([]);
-
-  function removeOneCharacter(index) {
-    const userToDelete = characters[index];
-
-    const promise = fetch("http://localhost:8000/users", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userToDelete),
-    }).then(() => {
-      setCharacters((prev) => prev.filter((_, i) => i !== index));
-    });
-  }
-
-  function updateList(person) {
-    postUser(person)
-      .then((res) => res.json())
-      .then((data) => setCharacters([...characters, data]))
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  function fetchUsers() {
-    const promise = fetch("http://localhost:8000/users");
-    return promise;
-  }
-
-  useEffect(() => {
-    fetchUsers()
-      .then((res) => res.json())
-      .then((json) => {
-        if (!Array.isArray(json)) {
-          json = [json];
-        }
-        setCharacters(json);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  function postUser(person) {
-    const promise = fetch("http://localhost:8000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(person),
-    });
-    return promise;
-  }
-
+export default function MyApp() {
   return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form handleSubmit={updateList} />
-    </div>
+    <Router>
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+	<Link to="/profile">Profile</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+	<Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default MyApp;
---
+++ packages/react-frontend/src/pages/About.jsx
@@ -0,0 +1,13 @@
+// src/pages/About.jsx
+export default function About() {
+  return (
+    <div className="page-container">
+      <h1>About OpenShelf</h1>
+      <p>
+        OpenShelf is a free digital library for copyright-free books, music, and media.
+        Our mission is to make open resources accessible to everyone.
+      </p>
+    </div>
+  );
+}
+
