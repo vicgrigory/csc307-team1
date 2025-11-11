127432a37c36f9edeabc720fca4901e2d19b8070
Tbohley <48847277+Tbohley@users.noreply.github.com>
2025-11-11 15:52:14 -0800

    Delete packages/express-backend/jest.config.js

--- packages/express-backend/jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  testEnvironment: "node",
-  setupFiles: ["<rootDir>/jest.env.js"],
-  testTimeout: 30000,
-  transform: {}, // needed when using ESM and experimental-vm-modules
-};
