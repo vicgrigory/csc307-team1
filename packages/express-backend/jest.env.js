2d04ff2d6b2caac08882be5846eba88cb55e1870
Tbohley <48847277+Tbohley@users.noreply.github.com>
2025-11-11 16:02:05 -0800

    Delete packages/express-backend/jest.env.js

--- packages/express-backend/jest.env.js
+++
@@ -1,7 +0,0 @@
-// jest.env.js
-import dotenv from "dotenv";
-
-// Make sure CI env vars aren't lost
-dotenv.config({ override: false });
-
-console.log("🧪 Jest setup: MONGODB_URI is", process.env.MONGODB_URI ? "defined" : "undefined");
