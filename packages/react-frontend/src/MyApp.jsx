a52fa01d58c30126181c7b80f73faef9780c6f6d
Matthew-Le-27 <mle79@calpoly.edu>
2025-11-26 13:12:11 -0800

    initial design for 'most-pop and recent-post; modified cards in home

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -2,8 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import MostPopular from "./pages/MostPopular";
+import RecentlyPosted from "./pages/RecentlyPosted";
+
 import Search from "./pages/Search";
 import AccountManagement from "./pages/AccountManagement";
+<<<<<<< Updated upstream
+=======
+import Login from "./pages/Login";
+
+>>>>>>> Stashed changes
 import NavBar from "./components/NavBar";
 
 function MyApp() {
@@ -29,20 +37,38 @@ function MyApp() {
           }
         />
         <Route
-          path="/search"
-          element={
-            <>
-              <NavBar />
-              <Search />
-            </>
-          }
-        />
-        <Route
           path="/profile"
           element={
             <>
               <NavBar />
               <Profile />
+            </>
+          }
+        />
+        <Route 
+          path="/popular"
+          element={
+            <>
+              <NavBar />
+              <MostPopular />
+            </>
+          }
+        />
+        <Route
+          path="/recentposted"
+          element={
+            <>
+              <NavBar />
+              <RecentlyPosted />
+            </>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <>
+              <NavBar />
+              <Search />
             </>
           }
         />
